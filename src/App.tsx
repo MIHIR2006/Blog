76eed6dea2dbf9bcdb5035e66377e491b0732cf2
MIHIR2006 <mihirgoswami2006@gmail.com>
2025-04-11 22:55:08 +0530

    Vercel Analytics Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ProgressBar } from "./components/ProgressBar";
-import Index from "./pages/Index";
+import About from "./pages/About";
 import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
-import About from "./pages/About";
+import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { Analytics } from '@vercel/analytics/react';
+
 
 const queryClient = new QueryClient();
 
@@ -35,6 +36,7 @@ const App = () => {
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
+        <Analytics />
       </TooltipProvider>
     </QueryClientProvider>
   );
